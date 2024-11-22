9c4eeec5abe9072555ee1d0036837ee53fea15fd
Chy-Zaber-Bin-Zahid <czaber47@gmail.com>
2024-11-22 12:50:42 +0600

    .env and authentication service done

--- authentication_service/main.py
+++ authentication_service/main.py
@@ -1,15 +1,18 @@
 import os
 from flask import Flask
 from routers.route import routes
+from flask_jwt_extended import JWTManager
 from flasgger import Swagger
 from dotenv import load_dotenv
 
 
 load_dotenv()
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 swagger = Swagger(app)
+jwt = JWTManager(app)
 routes(app)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('PORT_DESTINATION'))
+    app.run(debug=True, port=os.getenv('PORT_AUTHENTICATION'))
--- user_service/main.py
+++ user_service/main.py
@@ -1,15 +1,18 @@
+import os
 from flask import Flask
 from routers.route import routes
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger
+from dotenv import load_dotenv
 
 
+load_dotenv()
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 swagger = Swagger(app)
 routes(app)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=os.getenv('PORT_USER'))
